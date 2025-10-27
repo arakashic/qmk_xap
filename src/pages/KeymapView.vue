fe0545b8b25ff51d660e3bce4ce25d64c761f74e
Yanfei Guo <yanf.guo@gmail.com>
2025-10-27 11:52:30 -0500

    change keycode-button hover style to shade

--- src/pages/KeymapView.vue
+++ src/pages/KeymapView.vue
@@ -227,7 +227,7 @@
 }
 
 .keycode-button:hover {
-    width: 5rem !important;
-    height: 5rem !important;
+    background-color: rgba(0, 0, 0, 0.05);
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
 }
 </style>
